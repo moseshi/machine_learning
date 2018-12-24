22f973cd1c6db73d76b522703ee52eb03ff7584b
moseshi <moseshi@gmail.com>
2018-12-25 01:55:03 +0900

    log hozon

--- run_script_after_boot.sh
+++ run_script_after_boot.sh
@@ -5,9 +5,9 @@
 TARGET_PATH=/home/ec2-user/machine_learning
 su - ec2-user
 pushd $TARGET_PATH
-    git fetch origin develop
-    git checkout develop
-    git pull origin develop
+    git fetch origin develop 2>&1 >> result1
+    git checkout develop 2>&1 >> result1
+    git pull origin develop 2>&1 >> result1
     cat version > result2
     if [[ ! -f $TARGET_PATH/blocking_file ]]; then
 	echo "blocking file not found" >> result2
