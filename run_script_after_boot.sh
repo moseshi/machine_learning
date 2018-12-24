dad6ff48386fedad8f74110d85dfed2e846a8f71
moseshi <moseshi@gmail.com>
2018-12-25 02:00:50 +0900

    log file を別の場所に

--- run_script_after_boot.sh
+++ run_script_after_boot.sh
@@ -3,11 +3,12 @@
 # description: date logging
 # processname: date_logging
 TARGET_PATH=/home/ec2-user/machine_learning
+LOG_PATH=/home/ec2-user/log
 su - ec2-user
 pushd $TARGET_PATH
-    git fetch origin develop 2>&1 >> result1
-    git checkout develop 2>&1 >> result1
-    git pull origin develop 2>&1 >> result1
+    git fetch origin develop 2>&1 >> $LOG_PATH
+    git checkout develop 2>&1 >> $LOG_PATH
+    git pull origin develop 2>&1 >> $LOG_PATH
     cat version > result2
     if [[ ! -f $TARGET_PATH/blocking_file ]]; then
 	echo "blocking file not found" >> result2
