80c606c62931c0a8a6db0f9d1a9589ed6c83b506
moseshi <moseshi@gmail.com>
2019-01-08 02:10:31 +0900

    設定ファイルを変更

--- run_script_after_boot.sh
+++ run_script_after_boot.sh
@@ -2,11 +2,11 @@
 # chkconfig: 4 99 20
 # description: date logging
 # processname: date_logging
-TARGET_PATH=/home/ec2-user/machine_learning
-LOG_PATH=/home/ec2-user/log
-HOME=/home/ec2-user
+TARGET_PATH=/home/ubuntu/machine_learning
+LOG_PATH=/home/ubuntu/log
+HOME=/home/ubuntu
 pushd $TARGET_PATH
-    sudo -u ec2-user git pull origin develop 2>&1 >> $LOG_PATH
+    sudo -u ubuntu git pull origin develop 2>&1 >> $LOG_PATH
     /home/ubuntu/.local/share/virtualenvs/machine_learning-YCHg_Dnq/bin/python /home/ubuntu/machine_learning/main.py
     if [[ ! -f $TARGET_PATH/blocking_file ]]; then
 	shutdown 0
